7696382f6ee99e359ac448a3a3684bb0a808b860
Steve <stevekoech8@gmail.com>
2023-02-12 00:24:46 +0300

    Completed Assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+    def initialize(title)
+        @title = title
+    end
+    def title
+        @title
+    end
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+end
 
-
+book = Book.new("The Hitchhiker's Guide to the Galaxy")
